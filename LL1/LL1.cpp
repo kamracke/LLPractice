5481dafc663b2f894c817ae304e32fad638afe36
Kelsey McDonald <kamcdona@gmail.com>
2019-12-12 20:15:53 -0500

    Commit my changes

---
+++ LL1/LL1.cpp
@@ -0,0 +1,52 @@
+// LL1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
+
+Class Node{
+public
+	int data;
+	Node* next
+};
+
+int main()
+{
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
+
+	head = new Node();
+	second = new Node();
+	third = new Node();
+
+	head->data = 1;
+	head->next = second;
+
+	second->data = 2;
+	second->next = third
+
+	third->data = 3
+	third->next = NULL
+
+	return 0;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
